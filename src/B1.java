d5c1198e6021fc405905921ef071620eb772940c
Ivan Sadych <isadych@luxoft.com>
2019-09-03 12:50:09 +0300

    fix b1

---
+++ src/B1.java
@@ -0,0 +1,6 @@
+public class B1 {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+    }
+}
--- src/Master.java
+++ src/Master.java
@@ -2,8 +2,6 @@ public class Master {
     void main(String args[]) {
         System.out.printf("1");
         System.out.printf("2");
-        System.out.println("3");
-        System.out.println("4");
 
     }
 }
