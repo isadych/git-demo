d5f09b478d0bdf33c39ddc94cad85e0b41be9d89
Ivan Sadych <isadych@luxoft.com>
2019-09-03 12:58:14 +0300

    M_merge

--- src/Master.java
+++ src/Master.java
@@ -2,6 +2,9 @@ public class Master {
     void main(String args[]) {
         System.out.printf("1");
         System.out.printf("2");
+        System.out.println("3");
+        System.out.println("4");
+
 
 
     }
