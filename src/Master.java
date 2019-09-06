3f94ca35a95df57f78e4e335efabc11aa96afab0
isadych <54980557+isadych@users.noreply.github.com>
2019-09-06 12:15:56 +0300

    Update Master.java
    M_6

--- src/Master.java
+++ src/Master.java
@@ -4,6 +4,8 @@ public class Master {
         System.out.printf("2");
         System.out.println("3");
         System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
 
 
 
